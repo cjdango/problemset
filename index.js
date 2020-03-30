1464054644b0e9bb3ad7bf84a1f83eae7f739d81
cjdango <cjdango@gmail.com>
2020-03-30 14:26:45 +0800

    feat(stream): create RandStringSource class.

--- index.js
+++ index.js
@@ -14,3 +14,59 @@ const doAsync = async items => {
   }
 };
 
+
+class RandStringSource extends events.EventEmitter {
+  #remainder = '';
+
+  constructor(randStream) {
+    super();
+    this._listen(randStream);
+  }
+
+  _listen(randStream) {
+    randStream.on('data', chunk => {
+      this._emitData(chunk);
+    });
+  }
+
+  _emitData(chunk) {
+    let payload = '';
+    let emitCount = 0;
+
+    for (const char of chunk) {
+
+      // Encontered dot? Possible emit
+      if (char === '.') {
+
+        // Emit only if either payload or remainder is not empty.
+        if (payload || this.#remainder) {
+
+          // If this is gonna be the first emit for this chunk,
+          // then prepend remainder to the payload before emitting
+          if (emitCount === 0) {
+            this.emit('data', this.#remainder + payload);
+          }
+          else {
+            this.emit('data', payload);
+          }
+
+          // Reset remainder and payload for every emit
+          this.#remainder = '';
+          payload = '';
+
+          emitCount++;
+        }
+      }
+      // No emit needed, just append char
+      // to payload for next emit's use
+      else {
+        payload += char;
+      }
+
+    }
+
+    this.#remainder += payload;
+  }
+
+}
+
