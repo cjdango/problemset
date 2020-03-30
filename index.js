c7e1f05e9377fb565e55bdfa68b4110610f73a40
cjdango <cjdango@gmail.com>
2020-03-30 14:24:53 +0800

    feat(async): create doAsync function.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// solutions here
+'use strict';
+
+const lib = require('./lib/lib');
+const events = require('events');
+
+
+const doAsync = async items => {
+  for (const item of items) {
+    if (!Array.isArray(item)) {
+      await lib.asyncOp(item);
+    } else {
+      await Promise.all(item.map(element => lib.asyncOp(element)));
+    }
+  }
+};
+
